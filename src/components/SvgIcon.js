c0c22522511b3d3c89d66b97019862d094f9f2da
xiaoheng20190505 <1912272879@qq.com>
2020-07-05 22:29:10 +0800

    更新svg.js的注释；更新信息目录页面

--- src/components/SvgIcon.js
+++ src/components/SvgIcon.js
@@ -6,14 +6,21 @@ Vue.component('svgicon',SvgIcon)  //注册全局组件
 
 // 解析svg文件
 
-// require.context(‘../sources/icon’, false, /\.svg$/) 参数说明：
+// require.context(‘../sources/icon’, false, /\.svg$/) 
+//参数说明：
 // 第一个：目录
 // 第二个：是否遍历子级目录
 // 第三个：定义遍历文件规则
-
+/**
+ * @param {String} 第一个参数：目录
+ * @param {Boolean} 第二个参数：是否遍历子级目录
+ * @param {String} 第三个参数：定义遍历文件规则
+ */
 const req = require.context('../sources/icon', false, /\.svg$/)
 const requireAll = requireContext => {
-	// console.log(requireContext.keys().map(requireContext))
+	console.log(' requireContext ')
+	console.log( requireContext )
+	console.log(requireContext.keys().map(requireContext))
   return requireContext.keys().map(requireContext)
 }
 requireAll(req)
--- src/views/News/InfoCatagory.vue
+++ src/views/News/InfoCatagory.vue
@@ -1,8 +1,47 @@
 <template>
     <div>
-        信息目录
+        <el-select
+            v-model="value"
+            multiple
+            filterable
+            allow-create
+            default-first-option
+            @change="handleChange"
+            placeholder="请选择文章标签">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+            </el-option>
+        </el-select>
+        <input type="text" v-model="value">
     </div>
 </template>
 <style lang="scss" scoped>
 
 </style>
+<script>
+  export default {
+    data() {
+      return {
+        options: [{
+          value: 'HTML',
+          label: 'HTML'
+        }, {
+          value: 'CSS',
+          label: 'CSS'
+        }, {
+          value: 'JavaScript',
+          label: 'JavaScript'
+        }],
+        value: []
+      }
+    },
+    methods: {
+        handleChange(value) {
+            console.log(value)
+        }
+    }
+  }
+</script>
